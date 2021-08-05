e665b6cf9d93e43079ba6c182c70adbafb797815
egums <dima110.ru@gmail.com>
2021-08-05 20:32:11 +0300

    refactor constants.py

--- timetable_Genetic_Algorithm/utils/constants.py
+++ timetable_Genetic_Algorithm/utils/constants.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 MAX_PEDAGOG_LOAD_WEEK_IN_HOURS = 50
-MAX_HOURS_IN_WEEK_ONE = 21
-MAX_HOURS_IN_WEEK_TWO_THREE_FOUR = 23
-MAX_HOURS_IN_WEEK_FIVE = 29
-MAX_HOURS_IN_WEEK_SIX = 30
-MAX_HOURS_IN_WEEK_SEVEN = 32
-MAX_HOURS_IN_WEEK_EIGHT = 33
-MAX_HOURS_IN_WEEK_NINE = 36
-MAX_HOURS_IN_WEEK_ELEVEN_TEN = 37
+MAX_HOURS_IN_WEEK = {
+    1: 21,
+    2: 23,
+    3: 23,
+    4: 23,
+    5: 29,
+    6: 30,
+    7: 32,
+    8: 33,
+    9: 36,
+    10: 37,
+    11: 37
+}
 RUSSIAN_ALPHABET = np.array(["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С",
                              "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "ь", "Э", "Ю", "Я"])
