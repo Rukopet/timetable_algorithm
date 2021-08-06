96c4944da14203c924989b31a13439836fac5f55
egums <dima110.ru@gmail.com>
2021-08-06 16:56:47 +0300

    deleting stupid 7 g

--- timetable_Genetic_Algorithm/utils/algorithm_settings.py
+++ timetable_Genetic_Algorithm/utils/algorithm_settings.py
@@ -100,8 +100,7 @@ class AlgorithmSettings:
     @staticmethod
     def __fill_main_data_load_for_group(main_data: dict, keys: tuple, discipline: str,
                                         load: int, pedagogs_load: dict, sum_time: int):
-        if discipline in ["Иностранный язык (гр2)", "Иностранный язык (гр1)", "Обществознание", "Информатика (гр1)",
-                          "Информатика (гр2)"]:
+        if discipline in []:
             return
         group = main_data.get(keys)
         if group is None:
