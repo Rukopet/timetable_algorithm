a10d7514cc8fb11da556aaf182260927c2ba8d3b
egums <dima110.ru@gmail.com>
2021-08-06 18:38:24 +0300

    begin GeneratorLessons.py

---
+++ timetable_Genetic_Algorithm/utils/GeneratorLessons.py
@@ -0,0 +1,8 @@
+from timetable_Genetic_Algorithm.utils.algorithm_settings import AlgorithmSettings
+
+
+class GeneratorLessons:
+    def __int__(self, settings: AlgorithmSettings):
+        self._settings = settings
+
+
