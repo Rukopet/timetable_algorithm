61f790d46cba0bec6e0a0a8a43f8d604464cb5f3
egums <dima110.ru@gmail.com>
2021-08-07 15:52:18 +0300

    Lesson class + GeneratorLessons class

--- timetable_Genetic_Algorithm/utils/GeneratorLessons.py
+++ timetable_Genetic_Algorithm/utils/GeneratorLessons.py
@@ -1,8 +1,24 @@
 from timetable_Genetic_Algorithm.utils.algorithm_settings import AlgorithmSettings
 
 
+class Lesson:
+    """ intersection of main many; timeline unit """
+
+    def __init__(self, num_groups: int, letter_group: str, pedagog_name: str):
+        self.linked = False
+        self.locked = False
+        self.pedagog_name = pedagog_name
+        self.main_list = [num_groups, letter_group, pedagog_name]
+
+
 class GeneratorLessons:
+    """ main generator whole table, one on which individ """
+
     def __int__(self, settings: AlgorithmSettings):
         self._settings = settings
 
+    def gen_lesson(self):
+        pass
 
+    def gen_weak(self):
+        pass
