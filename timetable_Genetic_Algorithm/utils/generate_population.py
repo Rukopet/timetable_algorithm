272b6e2fe097282f60d0b4a594cc8b12d30e7755
dcordeli <dcordeli@student.21-school.ru>
2021-08-08 21:01:13 +0300

    nastuha krasava 3 methoda est

--- timetable_Genetic_Algorithm/utils/generate_population.py
+++ timetable_Genetic_Algorithm/utils/generate_population.py
@@ -32,9 +32,32 @@ def generateIndivid(table_settings: AlgorithmSettings):
             """
             if main -> audience -> 1 if 0 ->
             includetrust
-            elif auidinect -> 1 if 1 -> main_tuple[index][1] in 
+            elif auidinect -> 1 if 1 -> main_tuple[index][1] pizdushanya fucnya True -> 
+            
+            elif auidinect -> 1 if 2 -> found num letter -> checkin maintuple True | false
+            elif auidinect -> 1 if 3 -> 
             """
-            tmp[audience_tuple[audience]] = cur.getTupleIncludeWithTrust(trust_list, main_tuple)
+            if audience_tuple[audience][1] == 0:
+                tmp[audience_tuple[audience]] = cur.getTupleIncludeWithTrust(trust_list, main_tuple)
+            elif audience_tuple[audience][1] == 1:
+                trust_disciplines = RandomizerGenerationIncluded.getTrueListAudienceDisc(trust_list, main_tuple,
+                                                                                         audience_tuple[audience][0],
+                                                                                         table_settings)
+            #     TODO check all trust list on empty value
+            elif audience_tuple[audience][1] == 2:
+                trust_group = RandomizerGenerationIncluded.getTrueListAudienceGroup(trust_list, main_tuple,
+                                                                                    audience_tuple[audience][0],
+                                                                                    table_settings)
+                # print(trust_group)
+                # print(*[main_tuple[i] for i in trust_group])
+                # p = 1
+            elif audience_tuple[audience][1] == 3:
+                trust_mixed = RandomizerGenerationIncluded.getTrueListAudienceMix(trust_list, main_tuple,
+                                                                                    audience_tuple[audience][0],
+                                                                                    table_settings)
+                print(trust_mixed)
+                print(*[main_tuple[i] for i in trust_mixed])
+                p = 1
         pop[timeline] = tmp
         if cur.included_list_main_tuple.__len__() == 0:
             break
