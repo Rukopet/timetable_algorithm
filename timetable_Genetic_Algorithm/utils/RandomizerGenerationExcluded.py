c2ef3d90d677d8d488e50233556f98d113d3c68b
dcordeli <dcordeli@student.21-school.ru>
2021-08-08 20:00:02 +0300

    RandomizerGenerationExcluded class was deleted

--- timetable_Genetic_Algorithm/utils/RandomizerGenerationExcluded.py
+++ timetable_Genetic_Algorithm/utils/RandomizerGenerationExcluded.py
@@ -4,44 +4,6 @@ from timetable_Genetic_Algorithm.utils.algorithm_settings import AlgorithmSettin
 from timetable_Genetic_Algorithm.utils.constants import MAX_LESSONS_IN_DAY
 
 
-class RandomizerGenerationExcluded:
-    """ Simple randomizer for generate populations """
-
-    def __init__(self, len_main_tuple: int, len_audience_tuple: int):
-        self.excluded_list_main_tuple = []
-        self.excluded_list_audiences_tuple = []
-        self.len_main_tuple = len_main_tuple
-        self.len_audience_tuple = len_audience_tuple
-
-    def getNumExcludeRandomForMainTuple(self) -> int:
-        ret = choice([i for i in range(0, self.len_main_tuple) if i not in self.excluded_list_main_tuple])
-        self.excluded_list_main_tuple.append(ret)
-        return ret
-
-    def getNumExcludeRandomForAudienceTuple(self) -> int:
-        ret = choice([i for i in range(0, self.len_audience_tuple)
-                      if i not in self.excluded_list_audiences_tuple])
-        self.excluded_list_audiences_tuple.append(ret)
-        return ret
-
-    def getTupleExcludeRandomForMainTuple(self, main_tuple: list) -> tuple:
-        ret = choice([i for i in range(0, self.len_main_tuple) if i not in self.excluded_list_main_tuple])
-        self.excluded_list_main_tuple.append(ret)
-        return main_tuple[ret]
-
-    def getTupleExcludeRandomForAudienceTuple(self, audience_tuple: list) -> tuple:
-        ret = choice([i for i in range(0, self.len_audience_tuple)
-                      if i not in self.excluded_list_audiences_tuple])
-        self.excluded_list_audiences_tuple.append(ret)
-        return audience_tuple[ret]
-
-    def dropExcludedMainTuple(self):
-        self.excluded_list_main_tuple.clear()
-
-    def dropExcludedAudienceTuple(self):
-        self.excluded_list_audiences_tuple.clear()
-
-
 class RandomizerGenerationIncluded:
     """ Simple randomizer for generate populations """
 
