5eb4ab871405d71f315ada498df59df5a5616de7
Nasti <dcordeli@mail.com>
2021-08-10 14:07:58 +0300

    minor refactor

--- timetable_Genetic_Algorithm/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# from .custom_settings import DataFromFront
-# from .custom_settings import IDataFromFront
-# from .custom_settings import OurJsonClass
-#
-# from .algorithm_settings import AlgorithmSettings
-# from .constants import *
