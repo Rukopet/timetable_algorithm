c8d222affdeb1a84748c77d80838c8786dc65223
IvnAfanasiev <75682401+IvnAfanasiev@users.noreply.github.com>
2021-08-10 17:09:26 +0300

    Add files via upload

--- school/js/discipline-json.js
+++ school/js/discipline-json.js
@@ -1,4 +1,52 @@
 discipline = ["Математика", "Русский язык", "Литература", "Иностранный язык", "История", "Физическая культура", "Музыка", "Технология", "Химия", "Биология", "Физика", "Экология", "География", "Естествознание", "Астрономия", "Окружающий мир", "ИЗО", "Обществознание", "Информатика", "Геометрия"]
+classnum = {
+    "number": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    "letter": ["а", "б", "в", "г", "д"]
+}
+discipline.forEach((item, i, discipline) => {
+    $('#pair').append('<option value="pair1">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach1').append('<option value="discipline-teach1">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach2').append('<option value="discipline-teach2">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach3').append('<option value="discipline-teach3">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach4').append('<option value="discipline-teach4">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach5').append('<option value="discipline-teach5">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach6').append('<option value="discipline-teach6">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach7').append('<option value="discipline-teach7">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach8').append('<option value="discipline-teach8">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach9').append('<option value="discipline-teach9">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach10').append('<option value="discipline-teach10">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach11').append('<option value="discipline-teach11">' + discipline[i] + '</option>');
+});
+discipline.forEach((item, i, discipline) => {
+    $('#discipline-teach12').append('<option value="discipline-teach12">' + discipline[i] + '</option>');
+});
+
+// let classnum = document.getElementsById('classnum.number[i] + classnum.letter[i]');
+// classnum.forEach((item, i, classnum) => {
+//     $('#class-teach1').append('<option value="class-teach1">' + classnum.number[i] + classnum.letter[i] + '</option>');
+// });
 document.getElementById("discipline1").innerHTML = discipline[0];
 document.getElementById("discipline2").innerHTML = discipline[1];
 document.getElementById("discipline3").innerHTML = discipline[2];
@@ -20,7 +68,38 @@ document.getElementById("discipline18").innerHTML = discipline[17];
 document.getElementById("discipline19").innerHTML = discipline[18];
 document.getElementById("discipline20").innerHTML = discipline[19];
 
-discipline.forEach((item) => {
-    $('#pair').append('<option value="pair1">' + discipline[19] + '</option>');
-    $('#pair').append('<option value="pair2">' + discipline[18] + '</option>');
-});
+
+
+
+
+
+document.getElementById("class1a").innerHTML = classnum.number[0] + classnum.letter[0];
+document.getElementById("class1b").innerHTML = classnum.number[0] + classnum.letter[1];
+document.getElementById("class1c").innerHTML = classnum.number[0] + classnum.letter[2];
+document.getElementById("class2a").innerHTML = classnum.number[1] + classnum.letter[0];
+document.getElementById("class2b").innerHTML = classnum.number[1] + classnum.letter[1];
+document.getElementById("class2c").innerHTML = classnum.number[1] + classnum.letter[2];
+document.getElementById("class3a").innerHTML = classnum.number[2] + classnum.letter[0];
+document.getElementById("class3b").innerHTML = classnum.number[2] + classnum.letter[1];
+document.getElementById("class3c").innerHTML = classnum.number[2] + classnum.letter[2];
+document.getElementById("class4a").innerHTML = classnum.number[3] + classnum.letter[0];
+document.getElementById("class4b").innerHTML = classnum.number[3] + classnum.letter[1];
+document.getElementById("class4c").innerHTML = classnum.number[3] + classnum.letter[2];
+document.getElementById("class5a").innerHTML = classnum.number[4] + classnum.letter[0];
+document.getElementById("class5b").innerHTML = classnum.number[4] + classnum.letter[1];
+document.getElementById("class5c").innerHTML = classnum.number[4] + classnum.letter[2];
+document.getElementById("class6a").innerHTML = classnum.number[5] + classnum.letter[0];
+document.getElementById("class6b").innerHTML = classnum.number[5] + classnum.letter[1];
+document.getElementById("class6c").innerHTML = classnum.number[5] + classnum.letter[2];
+document.getElementById("class7a").innerHTML = classnum.number[6] + classnum.letter[0];
+document.getElementById("class7b").innerHTML = classnum.number[6] + classnum.letter[1];
+document.getElementById("class7c").innerHTML = classnum.number[6] + classnum.letter[2];
+document.getElementById("class8a").innerHTML = classnum.number[7] + classnum.letter[0];
+document.getElementById("class8b").innerHTML = classnum.number[7] + classnum.letter[1];
+document.getElementById("class8c").innerHTML = classnum.number[7] + classnum.letter[2];
+document.getElementById("class9a").innerHTML = classnum.number[8] + classnum.letter[0];
+document.getElementById("class9b").innerHTML = classnum.number[8] + classnum.letter[1];
+document.getElementById("class9c").innerHTML = classnum.number[8] + classnum.letter[2];
+document.getElementById("class10a").innerHTML = classnum.number[9] + classnum.letter[0];
+document.getElementById("class10b").innerHTML = classnum.number[9] + classnum.letter[1];
+document.getElementById("class11a").innerHTML = classnum.number[10] + classnum.letter[0];
