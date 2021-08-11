3259e28d316c942a8e98fb7304b729493ad980b6
egums <dima110.ru@gmail.com>
2021-08-11 19:47:25 +0300

    before merge

--- timetable_Genetic_Algorithm/utils/GeneratorLessons.py
+++ timetable_Genetic_Algorithm/utils/GeneratorLessons.py
@@ -3,24 +3,10 @@ import pandas as pd
 from timetable_Genetic_Algorithm.utils.algorithm_settings import AlgorithmSettings
 
 
-def util_diff_tuples(x):
-    return int(x[0]) - int(x[1])
 
 
-class Individ:
-    """ individ with reload __str__ """
 
-    def __init__(self, dict_ind: dict, settings: AlgorithmSettings):
-        self.dict_individ = dict_ind
-        self.settings = settings
 
-    # def __str__(self):
-    #     for day in range(6):
-    #         print("--------------------------" * 2, f'day {day}', "--------------------------" * 3, sep=' | ')
-    #         for timeline in range(self.settings.AMOUNT_TIMELINES_IN_DAY):
-    #             lst = [x for x in range(self.dict_individ[timeline])]
-    #             print(timeline, *lst, sep="\t|\t", end="--------------------------" * 3)
-    #         print()
 
 
 class Lesson:
@@ -41,8 +27,6 @@ class GeneratorLessons:
     def __init__(self, settings: AlgorithmSettings, week: list):
         self._settings = settings
         self.main_tuple_list = week
-        # self.main_dataDF = pd.DataFrame.from_dict(settings.main_data, )
-        # print(self.main_dataDF)
 
     def gen_lesson(self):
         pass
