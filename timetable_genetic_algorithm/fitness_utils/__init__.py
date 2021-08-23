f68276454351acc49793ee1494e4d580036e22c6
dcordeli <dcordeli@student.21-school.ru>
2021-08-23 14:17:46 +0300

    checkpoint before fix import err

--- timetable_genetic_algorithm/fitness_utils/__init__.py
+++ timetable_genetic_algorithm/fitness_utils/__init__.py
@@ -1,5 +1,4 @@
-from .our_typing import Rule, ModuleName, Timeline, Pedagog, AmountMentions, \
-NumberLessons, DayOfWeek, Discipline, TypeDiscipline, Type, List, Tuple, Any, Dict
+from .our_typing import Rule, ModuleName
 from .module_for_fitness_function_base import ModuleForFitnessFunctionBase
 from .module_registration_decorator import ModuleRegistration, module_register
 from .shared_soures import SharedData
