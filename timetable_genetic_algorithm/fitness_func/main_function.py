b5e332653b19441424755f66f25a648ec6ae68ee
egums <dima110.ru@gmail.com>
2021-09-03 22:11:25 +0300

    refactor logger

--- timetable_genetic_algorithm/fitness_func/main_function.py
+++ timetable_genetic_algorithm/fitness_func/main_function.py
@@ -18,7 +18,7 @@ def get_list_modules_object_list() -> List[ModuleForFitnessFunctionBase]:
     return ModuleRegistration().get_modules_object_list()
 
 
-def fitness_function(table_settings: AlgorithmSettings, individ: Individ):
+def fitness_function(table_settings: AlgorithmSettings, individ: Individ, log: LoggerUtils):
     shared_data = SharedData()
 
     # Initialization list of modules
@@ -26,21 +26,22 @@ def fitness_function(table_settings: AlgorithmSettings, individ: Individ):
     modules = get_list_modules_object_list()
     # shared_data.all_rules = generator_rules_matrix(modules)
 
-    log = LoggerUtils({individ.id_individ: {}})
-    name_dict_log = log.penalty[individ.id_individ]
+    # get ref on dict in log for current individ
+    current_individ_dict = log.penalty[individ.id_individ]
+
     for timeline in individ.dict_individ.keys():
         for auditory, lesson in individ.dict_individ[timeline].items():
             shared_data.current_timeline = timeline
             shared_data.current_audience = auditory
             shared_data.current_lesson = lesson
             for module in modules:
-                name_dict_log[module.get_module_naming()] = \
-                    name_dict_log.get(module.get_module_naming(), 0) + module.get_fitness_penalty()
+                current_individ_dict[module.get_module_naming()] = \
+                    current_individ_dict.get(module.get_module_naming(), 0) + module.get_fitness_penalty()
 
     sum_penalty = 0
     for module in modules:
-        name_dict_log[module.get_module_naming()] = \
-            name_dict_log.get(module.get_module_naming(), 0) + module.final_action()
-        # print(module.get_module_naming(), name_dict_log[module.get_module_naming()])
-        sum_penalty += name_dict_log[module.get_module_naming()]
-    print("Whole Penalty: ", sum_penalty)
+        current_individ_dict[module.get_module_naming()] = \
+            current_individ_dict.get(module.get_module_naming(), 0) + module.final_action()
+        sum_penalty += current_individ_dict[module.get_module_naming()]
+    current_individ_dict["sum"] = sum_penalty
+
--- timetable_genetic_algorithm/main_algorithm/main_loop_algorithm.py
+++ timetable_genetic_algorithm/main_algorithm/main_loop_algorithm.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 
 from timetable_genetic_algorithm.fitness_func.main_function import fitness_function
+from timetable_genetic_algorithm.logger.logger import LoggerUtils
 from timetable_genetic_algorithm.utils.Individ import Individ
 from timetable_genetic_algorithm.utils.algorithm_settings import AlgorithmSettings
 
@@ -9,7 +10,18 @@ def get_count_generation():
     return 200
 
 
+def generate_dict_for_logger(population: List[Individ]) -> Dict[int, Dict[str, int]]:
+    ret = {
+        ind.id_individ: {"instance": ind}
+        for ind in population
+    }
+    ret["max"] = None
+    return ret
+
+
 def main_loop(table_settings: AlgorithmSettings, population: List[Individ]):
+    log = LoggerUtils()
+    log.penalty = generate_dict_for_logger(population)
     for generation in range(get_count_generation()):
-        ...
-    fitness_function(table_settings, )
+        for individ in population:
+            fitness_function(table_settings, individ, log)
