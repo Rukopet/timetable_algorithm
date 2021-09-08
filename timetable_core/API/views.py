695cc223f1e9df1af63579652cbd0b5e316db90c
egums <dima110.ru@gmail.com>
2021-09-08 14:58:34 +0300

    adding models in django

---
+++ timetable_core/API/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic import View
+# Create your views here.
+
+
+class GroupsView(View):
+    def get(self, request, slug=None):
+        return render(request, 'API/groups.html')
+
+
+class AudiencesView(View):
+    def get(self, request, slug=None):
+        return render(request, 'API/audiences.html')
+
+
+class DisciplinesView(View):
+    def get(self, request, slug=None):
+        return render(request, 'API/disciplines.html')
+
+    def post(self):
+        ...
+
+
+class LoadPlanView(View):
+    def get(self, request, slug=None):
+        return render(request, 'API/loadplan.html')
+
+
+class PedagogsView(View):
+    def get(self, request, slug=None):
+        return render(request, 'API/pedagogs.html')
