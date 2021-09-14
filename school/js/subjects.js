5f44e4889280c747e316a07e086e93cb8cbec880
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2021-09-14 23:51:40 +0300

    dynamic loadhour page

--- school/js/subjects.js
+++ school/js/subjects.js
@@ -19,4 +19,25 @@ $(function() {
         innerParent.append("<div class='discipline-chek'> <input type='checkbox'  name='discipline" + i + "' " + " value='discipline" + i + "' " + " checked>     <input type='checkbox' name='discipline1-pair' value='discipline1-pair'> </div>");
     }    
 
+    
+
+    $("#subjects_next_page").click(function() {
+        console.log('Кнопка нажата!');
+        var subjects = getAllCurrent();
+        localStorage.setItem("subjects", JSON.stringify(subjects));        
+    })
+
 });
+
+function getAllCurrent() {
+
+    var arr = [];
+    $('.discipline_container').children('div').each(function () {
+        console.log(this); // "this" is the current element in the loop
+
+        var str = $( this).text().trim();
+        arr.push(str);        
+    });
+
+    return arr;  
+}
